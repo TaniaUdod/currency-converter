ff47d50499a2c245ca397fcdc8b733d73589fa53
TaniaUdod <taniaudod28@gmail.com>
2024-02-28 00:04:14 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,9 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/currency-converter/",
+  base: "/",
   plugins: [react()],
+  build: {
+    outDir: "dist",
+  },
 });
