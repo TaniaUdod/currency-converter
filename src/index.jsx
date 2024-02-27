7951d1ee6ff6ba899206a9a64e417542d8e3f980
TaniaUdod <taniaudod28@gmail.com>
2024-02-27 23:43:40 +0200

    update

--- src/index.jsx
+++ src/index.jsx
@@ -4,9 +4,9 @@ import "@fontsource/roboto/300.css";
 import "@fontsource/roboto/400.css";
 import "@fontsource/roboto/500.css";
 import "@fontsource/roboto/700.css";
-import "./src/index.css";
-import App from "./src/App.jsx";
-import CurrencyProvider from "./src/context/CurrencyContext.jsx";
+import "./index.css";
+import App from "./App.jsx";
+import CurrencyProvider from "./context/CurrencyContext.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
